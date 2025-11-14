f64f08f90569af4b7fa61ffbf229dc48c2100807
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-11-15 00:00:29 +0900

    [Feat] Editor 스크립트 추가

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+class MyEditorScript
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static void PerformBuild()
+    {
+        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Windows/Gomoku.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+}
